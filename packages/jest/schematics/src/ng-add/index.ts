ad2ced1071c4e9566bf61fa6a1c2e1fd80cda97e
Valdis Thomann <valdis.thomann@gmail.com>
2019-02-01 15:18:25 +1100

    Added schmatic and copied relevant parts from froko/ng-essentials

---
+++ packages/jest/schematics/src/ng-add/index.ts
@@ -0,0 +1,117 @@
+import {
+  Rule,
+  chain,
+  Tree,
+  SchematicContext
+} from "@angular-devkit/schematics";
+
+export const ANGULAR_JSON = 'angular.json';
+export const TSCONFIG = './tsconfig.json';
+
+import {
+  deleteFile,
+  removePackageFromPackageJson,
+  addPackageToPackageJson,
+  editTsConfigSpecJson,
+  runNpmPackageInstall
+} from "../utils";
+
+export function addJest(): Rule {
+  return chain([
+    // removePackageFromPackageJson("devDependencies", "@types/jasmine"),
+    // removePackageFromPackageJson("devDependencies", "jasmine-core"),
+    // removePackageFromPackageJson("devDependencies", "jasmine-spec-reporter"),
+    removePackageFromPackageJson("devDependencies", "karma"),
+    removePackageFromPackageJson("devDependencies", "karma-chrome-launcher"),
+    removePackageFromPackageJson(
+      "devDependencies",
+      "karma-coverage-istanbul-reporter"
+    ),
+    removePackageFromPackageJson("devDependencies", "karma-jasmine"),
+    removePackageFromPackageJson(
+      "devDependencies",
+      "karma-jasmine-html-reporter"
+    ),
+    deleteFile("src/karma.conf.js"),
+    deleteFile("src/test.ts"),    
+    addPackageToPackageJson("devDependencies", "jest"),
+    addPackageToPackageJson(
+      "devDependencies",
+      "@angular-builders/jest"
+    ),
+    // addPackageToPackageJson(
+    //   "devDependencies",
+    //   "@types/jest"
+    // ),
+    // addPackageToPackageJson(
+    //   "devDependencies",
+    //   "babel-core"
+    // ),
+    // addPackageToPackageJson(
+    //   "devDependencies",
+    //   "babel-jest"
+    // ),
+    // addPackageToPackageJson(
+    //   "devDependencies",
+    //   "jest-preset-angular"
+    // ),
+    runNpmPackageInstall(),    
+    editTsConfigSpecJson("src"),    
+    editTsConfigRootJson(),
+    switchToJestBuilderInAngularJson(),
+    // createLaunchJson(),
+    // copyConfigFiles("jest")
+  ]);
+}
+
+function switchToJestBuilderInAngularJson(): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(ANGULAR_JSON)) {
+      return host;
+    }
+
+    const sourceText = host.read(ANGULAR_JSON).toString("utf-8");
+    const angularJson = JSON.parse(sourceText);
+    const defaultProject = angularJson["defaultProject"];
+
+    if (
+      angularJson["projects"][defaultProject]["architect"]["test"]["builder"]
+    ) {
+      angularJson["projects"][defaultProject]["architect"]["test"]["builder"] =
+        "@angular-builders/jest:run";
+    }
+
+    if (
+      angularJson["projects"][defaultProject]["architect"]["test"]["options"]
+    ) {
+      angularJson["projects"][defaultProject]["architect"]["test"][
+        "options"
+      ] = {};
+    }
+
+    host.overwrite(ANGULAR_JSON, JSON.stringify(angularJson, null, 2));
+
+    return host;
+  };
+}
+
+function editTsConfigRootJson(): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(TSCONFIG)) {
+      return host;
+    }
+
+    const sourceText = host.read(TSCONFIG).toString("utf-8");
+    const tsconfigJson = JSON.parse(sourceText);
+
+    if (!tsconfigJson["exclude"]) {
+      tsconfigJson["exclude"] = [];
+    }
+
+    tsconfigJson["exclude"] = ["**/*.spec.ts", "setup-jest.ts"];
+
+    host.overwrite(TSCONFIG, JSON.stringify(tsconfigJson, null, 2));
+
+    return host;
+  };
+}
---
+++ packages/jest/schematics/src/utils.ts
@@ -0,0 +1,275 @@
+import {
+  apply,
+  MergeStrategy,
+  mergeWith,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url
+} from "@angular-devkit/schematics";
+import { strings } from "@angular-devkit/core";
+import { NodePackageInstallTask } from "@angular-devkit/schematics/tasks";
+
+const PACKAGE_JSON = "package.json";
+
+export function removeAutomaticUpdateSymbols(): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(PACKAGE_JSON)) {
+      return host;
+    }
+
+    const sourceText = host.read(PACKAGE_JSON).toString("utf-8");
+    const packageJson = JSON.parse(sourceText);
+
+    for (const index in packageJson.dependencies) {
+      packageJson.dependencies[index] = packageJson.dependencies[index].replace(
+        "^",
+        ""
+      );
+      packageJson.dependencies[index] = packageJson.dependencies[index].replace(
+        "~",
+        ""
+      );
+      packageJson.dependencies[index] = packageJson.dependencies[index].replace(
+        ">=",
+        ""
+      );
+    }
+
+    for (const index in packageJson.devDependencies) {
+      packageJson.devDependencies[index] = packageJson.devDependencies[
+        index
+      ].replace("^", "");
+      packageJson.devDependencies[index] = packageJson.devDependencies[
+        index
+      ].replace("~", "");
+      packageJson.devDependencies[index] = packageJson.devDependencies[
+        index
+      ].replace(">=", "");
+    }
+
+    host.overwrite(PACKAGE_JSON, JSON.stringify(packageJson, null, 2));
+
+    return host;
+  };
+}
+
+export function removePackageFromPackageJson(type: string, pkg: string): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(PACKAGE_JSON)) {
+      return host;
+    }
+
+    const sourceText = host.read(PACKAGE_JSON).toString("utf-8");
+    const packageJson = JSON.parse(sourceText);
+
+    if (!packageJson[type]) {
+      return host;
+    }
+
+    delete packageJson[type][pkg];
+
+    host.overwrite(PACKAGE_JSON, JSON.stringify(packageJson, null, 2));
+
+    return host;
+  };
+}
+
+export function addPackageToPackageJson(
+  type: string,
+  pkg: string,
+  version?: string
+): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(PACKAGE_JSON)) {
+      return host;
+    }
+
+    const sourceText = host.read(PACKAGE_JSON).toString("utf-8");
+    const packageJson = JSON.parse(sourceText);
+
+    if (!packageJson[type]) {
+      packageJson[type] = {};
+    }
+
+    if (!packageJson[type][pkg]) {
+      packageJson[type][pkg] = version;
+    }
+
+    if (version && !packageJson[type][pkg][version]) {
+      packageJson[type][pkg] = version;
+    }
+
+    host.overwrite(PACKAGE_JSON, JSON.stringify(packageJson, null, 2));
+
+    return host;
+  };
+}
+
+export function updatePackageInPackageJson(
+  type: string,
+  pkg: string,
+  version: string
+): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(PACKAGE_JSON)) {
+      return host;
+    }
+
+    const sourceText = host.read(PACKAGE_JSON).toString("utf-8");
+    const packageJson = JSON.parse(sourceText);
+
+    if (!packageJson[type]) {
+      packageJson[type] = {};
+    }
+
+    if (packageJson[type][pkg]) {
+      packageJson[type][pkg] = version;
+    }
+
+    host.overwrite(PACKAGE_JSON, JSON.stringify(packageJson, null, 2));
+
+    return host;
+  };
+}
+
+export function removeScriptFromPackageJson(key: string): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(PACKAGE_JSON)) {
+      return host;
+    }
+
+    const sourceText = host.read(PACKAGE_JSON).toString("utf-8");
+    const packageJson = JSON.parse(sourceText);
+
+    if (!packageJson["scripts"]) {
+      packageJson["scripts"] = {};
+    }
+
+    if (packageJson["scripts"][key]) {
+      delete packageJson["scripts"][key];
+    }
+
+    host.overwrite(PACKAGE_JSON, JSON.stringify(packageJson, null, 2));
+
+    return host;
+  };
+}
+
+export function addScriptToPackageJson(key: string, command: string): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(PACKAGE_JSON)) {
+      return host;
+    }
+
+    const sourceText = host.read(PACKAGE_JSON).toString("utf-8");
+    const packageJson = JSON.parse(sourceText);
+
+    if (!packageJson["scripts"]) {
+      packageJson["scripts"] = {};
+    }
+
+    packageJson["scripts"] = {
+      ...Object.keys(packageJson["scripts"])
+        .filter(existingKey => existingKey !== key)
+        .reduce((obj, key) => {
+          obj[key] = packageJson["scripts"][key];
+          return obj;
+        }, {}),
+      [key]: command
+    };
+
+    host.overwrite(PACKAGE_JSON, JSON.stringify(packageJson, null, 2));
+
+    return host;
+  };
+}
+
+export function editTsConfigLibJson(path: string): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(`${path}/tsconfig.lib.json`)) {
+      return host;
+    }
+
+    const sourceText = host.read(`${path}/tsconfig.lib.json`).toString("utf-8");
+    const tsconfigJson = JSON.parse(sourceText);
+
+    if (!tsconfigJson["exclude"]) {
+      tsconfigJson["exclude"] = [];
+    }
+
+    tsconfigJson["exclude"] = ["**/*.spec.ts"];
+
+    host.overwrite(
+      `${path}/tsconfig.lib.json`,
+      JSON.stringify(tsconfigJson, null, 2)
+    );
+
+    return host;
+  };
+}
+
+export function editTsConfigSpecJson(path: string): Rule {
+  return (host: Tree, _: SchematicContext) => {
+    if (!host.exists(`${path}/tsconfig.spec.json`)) {
+      return host;
+    }
+
+    const sourceText = host
+      .read(`${path}/tsconfig.spec.json`)
+      .toString("utf-8");
+    const tsconfigJson = JSON.parse(sourceText);
+
+    if (tsconfigJson["files"]) {
+      delete tsconfigJson["files"];
+    }
+
+    if (!tsconfigJson["compilerOptions"]["types"]) {
+      tsconfigJson["compilerOptions"]["types"] = [];
+    }
+
+    tsconfigJson["compilerOptions"]["types"] = ["jest", "node"];
+
+    if (!tsconfigJson["compilerOptions"]["module"]) {
+      tsconfigJson["compilerOptions"]["module"] = "";
+    }
+
+    tsconfigJson["compilerOptions"]["module"] = "commonjs";
+
+    host.overwrite(
+      `${path}/tsconfig.spec.json`,
+      JSON.stringify(tsconfigJson, null, 2)
+    );
+
+    return host;
+  };
+}
+
+export function deleteFile(file: string) {
+  return (host: Tree, _: SchematicContext) => {
+    host.delete(file);
+
+    return host;
+  };
+}
+
+export function copyConfigFiles(path: string): Rule {
+  return mergeWith(
+    apply(url(path), [
+      template({
+        utils: strings,
+        dot: ".",
+        tmpl: ""
+      })
+    ]),
+    MergeStrategy.Overwrite
+  );
+}
+
+export function runNpmPackageInstall() {
+  return (host: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    return host;
+  };
+}
